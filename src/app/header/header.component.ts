827dc60272f05f3ee70d6ee9c1e5460f1b25ced3
Felipe Almeida <felipepereira937@gmail.com>
2023-10-26 14:49:34 -0300

    split page components: header and footer

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'weaver-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
